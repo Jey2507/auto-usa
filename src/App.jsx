55656f2d4f05b733e3ebeb72323f12e242ae5760
Oleh Toporovych <130228552+Jey2507@users.noreply.github.com>
2025-01-26 21:54:41 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { lazy } from "react";
+import { Routes, Route} from 'react-router-dom';
+
+const Layout = lazy(() => import("./components/Layout/Layout"))
+const MovieCast = lazy(() => import("./components/MovieCast/MovieCast"))
+const MovieReviews = lazy(() => import("./components/MovieReviews/MovieReviews"))
+const HomePage = lazy(() => import("./pages/HomePage/HomePage"))
+const MoviesPage = lazy(() => import("./pages/MoviesPage/MoviesPage"))
+const NotFoundPage = lazy(() => import("./pages/NotFoundPage/NotFoundPage"))
+const MovieDetailsPage = lazy(() => import("./pages/MovieDetailsPage/MovieDetailsPage"))
+
+import './App.css';
+
+export default function App() {
+  return (
+    <Layout>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/movies' element={<MoviesPage />} />
+        <Route path='/movies/:movieId' element={<MovieDetailsPage />}>
+          <Route path='cast' element={<MovieCast />} />
+          <Route path='reviews' element={<MovieReviews />} />
+        </Route>
+        <Route path='*' element={<NotFoundPage />} />
+      </Routes>
+    </Layout>
+  );
+}
