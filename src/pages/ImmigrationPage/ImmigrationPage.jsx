1a044960d234410538828d4bac23184776d14e9e
Oleh Toporovych <otoporovych2@gmail.com>
2025-02-10 20:24:02 +0100

    update page

--- src/pages/ImmigrationPage/ImmigrationPage.jsx
+++ src/pages/ImmigrationPage/ImmigrationPage.jsx
@@ -1,11 +1,13 @@
 import AboutUsImmigration from "../../components/AboutUsImmigration/AboutUsImmigration";
 import FaqImmigration from "../../components/FaqImmigration/FaqImmigration";
+import OnePartPage from "../../components/OnePartPage/OnePartPage";
 
 import ImmigrationServices from "../../components/ServicesImmigration/ServicesImmigration";
 // import css from "./ImmigrationPage.module.css"
 export default function ImmigrationPage() {
     return (
         <>
+            <OnePartPage />
             <AboutUsImmigration />
             <ImmigrationServices/>
             <FaqImmigration/>
