bdf99ff66b33502e501ee3599ee28bf461b1a818
Oleh Toporovych <otoporovych2@gmail.com>
2025-02-11 20:46:31 +0100

    Update ImmigrationPage.jsx

--- src/pages/ImmigrationPage/ImmigrationPage.jsx
+++ src/pages/ImmigrationPage/ImmigrationPage.jsx
@@ -1,8 +1,8 @@
 // import AboutUsImmigration from "../../components/AboutUsImmigration/AboutUsImmigration";
-import FaqImmigration from "../../components/FaqImmigration/FaqImmigration";
-import PricesImmigration from "../../components/PricesImmigration/PricesImmigration";
-
-import ImmigrationServices from "../../components/ServicesImmigration/ServicesImmigration";
+import FaqImmigration from "../../components/FaqImmigration/FaqImmigration.jsx";
+import PricesImmigration from "../../components/PricesImmigration/PricesImmigration.jsx";
+import OnePartPage from "../../components/OnePartPage/OnePartPage.jsx";
+import ImmigrationServices from "../../components/ServicesImmigration/ServicesImmigration.jsx";
 export default function ImmigrationPage() {
     return (
         <>
