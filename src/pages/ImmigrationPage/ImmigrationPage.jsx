50280be3459b32018f5954885abecfa11500d43f
Petro Podolets <petropodolets7@gmail.com>
2025-02-11 20:16:47 +0100

    price

--- src/pages/ImmigrationPage/ImmigrationPage.jsx
+++ src/pages/ImmigrationPage/ImmigrationPage.jsx
@@ -1,13 +1,14 @@
 import AboutUsImmigration from "../../components/AboutUsImmigration/AboutUsImmigration";
 import FaqImmigration from "../../components/FaqImmigration/FaqImmigration";
+import PricesImmigration from "../../components/PricesImmigration/PricesImmigration";
 
 import ImmigrationServices from "../../components/ServicesImmigration/ServicesImmigration";
-// import css from "./ImmigrationPage.module.css"
 export default function ImmigrationPage() {
     return (
         <>
             <AboutUsImmigration />
-            <ImmigrationServices/>
+            <ImmigrationServices />
+            <PricesImmigration/>
             <FaqImmigration/>
         </>
                 
