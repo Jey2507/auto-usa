982f4344395acca26ea52cb1e778c1027af4a07b
Oleh Toporovych <otoporovych2@gmail.com>
2025-02-24 21:56:25 +0100

    update last upppdattee??

--- src/pages/ImmigrationPage/ImmigrationPage.jsx
+++ src/pages/ImmigrationPage/ImmigrationPage.jsx
@@ -1,6 +1,7 @@
 import AboutUsImmigration from "../../components/AboutUsImmigration/AboutUsImmigration.jsx";
 import FaqImmigration from "../../components/FaqImmigration/FaqImmigration.jsx";
 import OnePartPage from "../../components/OnePartPage/OnePartPage.jsx";
+import ReviewFoto from "../../components/ReviewFoto/ReviewFoto.jsx";
 import ImmigrationServices from "../../components/ServicesImmigration/ServicesImmigration.jsx";
 export default function ImmigrationPage() {
     return (
@@ -8,6 +9,7 @@ export default function ImmigrationPage() {
             <OnePartPage/>
             <ImmigrationServices />
             <AboutUsImmigration />
+            <ReviewFoto />
             <FaqImmigration/>
         </>
                 
