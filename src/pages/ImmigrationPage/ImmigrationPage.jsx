01c5d2f74756c2e3c8fff199046eedf3c6755e30
Oleh Toporovych <otoporovych2@gmail.com>
2025-02-24 21:35:57 +0100

    finish update?

--- src/pages/ImmigrationPage/ImmigrationPage.jsx
+++ src/pages/ImmigrationPage/ImmigrationPage.jsx
@@ -3,6 +3,7 @@ import FaqImmigration from "../../components/FaqImmigration/FaqImmigration.jsx";
 import OnePartPage from "../../components/OnePartPage/OnePartPage.jsx";
 import Review from "../../components/Review/Review.jsx";
 import ImmigrationServices from "../../components/ServicesImmigration/ServicesImmigration.jsx";
+
 export default function ImmigrationPage() {
     return (
         <>
