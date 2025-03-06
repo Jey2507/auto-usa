4b46b7a0a4cdf2c5681d579b2928c14d4bf8e5f7
Oleh Toporovych <otoporovych2@gmail.com>
2025-03-06 11:17:45 +0100

    create last section on main page

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,26 +1,34 @@
-import { lazy } from "react";
-import { Routes, Route} from 'react-router-dom';
-import '../App/App.module.css';
+import { lazy, Suspense } from "react";
+import { Routes, Route } from "react-router-dom";
+import "../App/App.module.css";
 import Layout from "../Layout/Layout.jsx";
 
-const HomePage = lazy(() => import("../../pages/HomePage/HomePage.jsx"))
-const MarketingPage = lazy(() => import("../../pages/MarketingPage/MarketingPage.jsx"))
-const ImmigrationPage = lazy(() => import("../../pages/ImmigrationPage/ImmigrationPage.jsx"))
-const OnlineBusiness = lazy(() => import("../../pages/OnlineBusiness/OnlineBusiness.jsx"))
-const AutoPage = lazy(() => import("../../pages/AutoPage/AutoPage.jsx"))
-const NotFoundPage = lazy(() => import("../../pages/NotFoundPage/NotFoundPage"))
+const HomePage = lazy(() => import("../../pages/HomePage/HomePage.jsx"));
+const MarketingPage = lazy(() => import("../../pages/MarketingPage/MarketingPage.jsx"));
+const ImmigrationPage = lazy(() => import("../../pages/ImmigrationPage/ImmigrationPage.jsx"));
+const OnlineBusiness = lazy(() => import("../../pages/OnlineBusiness/OnlineBusiness.jsx"));
+const AutoPage = lazy(() => import("../../pages/AutoPage/AutoPage.jsx"));
+const AboutMe = lazy(() => import("../../components/AboutMe/AboutMe.jsx"));
+const Socials = lazy(() => import("../../components/Socials/Socials.jsx"));
+const NotFoundPage = lazy(() => import("../../pages/NotFoundPage/NotFoundPage"));
 
 export default function App() {
   return (
     <Layout>
-      <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/imigration' element={<ImmigrationPage />} />
-        <Route path='/marketing' element={<MarketingPage />} />
-        <Route path='/business' element={<OnlineBusiness />} />
-        <Route path='/auto' element={<AutoPage />} />
-        <Route path='*' element={<NotFoundPage />} />
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<HomePage />}>
+            <Route index element={<AboutMe />} />
+            <Route path="aboutme" element={<AboutMe />} />
+            <Route path="socials" element={<Socials />} />
+          </Route>
+          <Route path="/imigration" element={<ImmigrationPage />} />
+          <Route path="/marketing" element={<MarketingPage />} />
+          <Route path="/business" element={<OnlineBusiness />} />
+          <Route path="/auto" element={<AutoPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </Suspense>
     </Layout>
   );
 }
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,52 +1,82 @@
-import { NavLink } from "react-router-dom";
-import css from "../../pages/HomePage/HomePage.module.css"
+import { NavLink, Outlet } from "react-router-dom";
+import css from "../../pages/HomePage/HomePage.module.css";
+import clsx from "clsx";
+import { Suspense } from "react";
+
+const getNavLinkClass = ({ isActive }) => {
+  return clsx(css.link, isActive && css.active);
+};
 
 export default function HomePage() {
-    return (
-        <section>
-            <div className={css.mainBox}>
-                <div>
-                    <h1>Zoia Kibysh</h1>
-                    <h2>Influencer | Marketer in USA</h2>
-                </div>
-                <p>Являюсь владельцем Marketing компании в США “Royal Business Solution”
-                Специализируюсь в иммиграции и адаптации в США
-                Являюсь экспертом по покупке и продаже автомобилей через автоаукционы США</p>
-            </div>
-            <div className={css.twoBox}>
-                <h2>УСЛУГИ И НАПРАВЛЕНИЯ:</h2>
-                <ul>
-                    
-                    <li>
-                    <NavLink to="/imigration">
-                        <h3>ИММИГРАЦИЯ</h3>
-                        <p>Моя команда пошагово помогает с переездом в США, адаптации по приезду, получением всех необходимых документов и дальнейшей легализации</p>
-                    </NavLink>
-                    </li>
+  return (
+    <section>
+      <div className={css.mainBox}>
+        <div>
+          <h1>Zoia Kibysh</h1>
+          <h2>Influencer | Marketer in USA</h2>
+        </div>
+        <p>
+          Являюсь владельцем Marketing компании в США “Royal Business Solution” Специализируюсь в
+          иммиграции и адаптации в США Являюсь экспертом по покупке и продаже автомобилей через
+          автоаукционы США
+        </p>
+      </div>
+      <div className={css.twoBox}>
+        <h2>УСЛУГИ И НАПРАВЛЕНИЯ:</h2>
+        <ul>
+          <li>
+            <NavLink to="/imigration">
+              <h3>ИММИГРАЦИЯ</h3>
+              <p>
+                Моя команда пошагово помогает с переездом в США, адаптации по приезду, получением
+                всех необходимых документов и дальнейшей легализации
+              </p>
+            </NavLink>
+          </li>
 
-                    <li>
-                        <NavLink to="/marketing">
-                        <h3>МАРКЕТИНГ</h3>
-                        <p>Моя команда эффективно упаковывает продукт/услуги, разрабатывает стратегию развития и успешно продвигает бизнеса на рынке США и СНГ, а также обучаем направлению СММ и маркетинг.
-                        </p>
-                        </NavLink>
-                    </li>
-                    
-                    
-                    <li>
-                    <NavLink to="/auto">
-                        <h3>АВТОАУКЦИОНЫ</h3>
-                        <p>Покупаем и продаем автомобили через аукционы США, а также обучаем этому направлению!</p>
-                    </NavLink>
-                    </li>
-                    <li>
-                    <NavLink to="/business">
-                        <h3>Online Business</h3>
-                        <p>Больше информации об иммиграции и возможностях, которые мы предлагаем для открытия бизнеса</p>
-                    </NavLink>
-                    </li>
-                </ul>
-            </div>
-        </section>
-    )
+          <li>
+            <NavLink to="/marketing">
+              <h3>МАРКЕТИНГ</h3>
+              <p>
+                Моя команда эффективно упаковывает продукт/услуги, разрабатывает стратегию развития
+                и успешно продвигает бизнеса на рынке США и СНГ, а также обучаем направлению СММ и
+                маркетинг.
+              </p>
+            </NavLink>
+          </li>
+
+          <li>
+            <NavLink to="/auto">
+              <h3>АВТОАУКЦИОНЫ</h3>
+              <p>Покупаем и продаем автомобили через аукционы США, а также обучаем этому направлению!</p>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/business">
+              <h3>Online Business</h3>
+              <p>Больше информации об иммиграции и возможностях, которые мы предлагаем для открытия бизнеса</p>
+            </NavLink>
+          </li>
+        </ul>
+        <div className={css.centerBox}>
+          <a href="https://t.me/zoia_kibysh" target="_blank" className={css.ctaButton} rel="noreferrer">
+            Подробнее
+          </a>
+        </div>
+      </div>
+      <div>
+        <ul className={css.listHeader}>
+          <li>
+            <NavLink to="aboutme" className={getNavLinkClass}>Обо мне</NavLink>
+          </li>
+          <li>
+            <NavLink to="socials" className={getNavLinkClass}>Реклама</NavLink>
+          </li>
+        </ul>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Outlet />
+        </Suspense>
+      </div>
+    </section>
+  );
 }
